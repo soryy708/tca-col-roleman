84ed7bde390ec156b336b47a073790375609bd97
Ivan Rubinson <soryy708@gmail.com>
2019-12-20 14:19:31 +0200

    Impl setUserRoles

--- util/discord.js
+++ util/discord.js
@@ -1,3 +1,5 @@
+const request = require('request');
+
 function getUserName(bot, serverId, str) {
     const server = bot.servers[serverId];
     const userMatch = Object.values(bot.users).find(user => user.username.toLowerCase() === str.toLowerCase());
@@ -11,6 +13,40 @@ function getUserName(bot, serverId, str) {
     return null;
 }
 
+function setUserRoles(serverId, userId, roles) {
+    // https://github.com/izy521/discord.io/issues/289#issuecomment-418552716
+    return new Promise((resolve, reject) => {
+        request({
+            url: `https://discordapp.com/api/v6/guilds/${serverId}/members/${userId}`,
+            headers: {
+                'User-Agent': 'DiscordBot (Custom API request, 1.0)',
+                'Authorization': `Bot ${process.env.DISCORD_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            method: 'PATCH',
+            body: JSON.stringify({roles: roles})
+        }, (error, response, body) => {
+            if (error) {
+                reject(error);
+            }
+            if (response.statusCode !== 204) {
+                if (response.statusCode === 429) {
+                    const retryTimeout = body.retry_after;
+                    setTimeout(() => {
+                        setUserRoles(serverId, userId, roles)
+                            .then(body => resolve(body))
+                            .catch(e => reject(e));
+                    }, retryTimeout);
+                } else {
+                    reject(body);
+                }
+            }
+            resolve(body);
+        });
+    });
+}
+
 module.exports = {
     getUserName,
+    setUserRoles,
 };
